6413932e79513e48175039735ab1a99ac88421fe
Jay Wang <jay.wang@sap.com>
2017-05-26 18:06:39 +0800

    support analytical table

--- webapp/controller/GroupShare.controller.js
+++ webapp/controller/GroupShare.controller.js
@@ -1,18 +1,35 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function(Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel"
+], function(Controller,JSONModel) {
 	"use strict";
 
 	return Controller.extend("fin.confin.con.groupshare.controller.GroupShare", {
 		onInit:function(){
 
+			var aFiscalYear =  {	data:[ {  key:'2017.001',value:'2017.001'  },
+										{  key:'2017.002',value:'2017.002'  },
+										{  key:'2017.003',value:'2017.003'  },
+										{  key:'2017.004',value:'2017.004'  },
+										{  key:'2017.005',value:'2017.005'  },
+										{  key:'2017.006',value:'2017.006'  }
+								]};
+
+			  var oFiscalModel = new JSONModel(aFiscalYear);
+			  this.getView().setModel(oFiscalModel,"fiscalYear");
+
+			 var oTable = this.getView().byId('activityList');
+			//  var oDefaultModel = this.getModel();
+			oTable.bindRows({path:'/InvestmentActivitys'});
+
+			
 		},
 
 		onSearch:function(event){
 
-			console.log(event);
+
 			var oTable = this.byId("activityList");
-			oTable.getBinding("groupshare>/InvestmentActivitys").filter([]);
+			// oTable.getBinding("groupshare>/InvestmentActivitys").filter([]);
 
 		},
 		onBefore:function(oEvent){
@@ -23,7 +40,7 @@ sap.ui.define([
 		// onBeforeRebind:function(oEvent){
 
 
-
+ 
 		// }
 	});
 });
